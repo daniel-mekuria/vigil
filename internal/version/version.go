6ba3bf4b2abb2eba00825f284efcddaff5fee452
daniel-mekuria <mekuriadaniel4@gmail.com>
2026-07-20 18:05:10 -0700

    release: bump main to v0.1.3-dev

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the StatLite build version.
 // Release tags use vX.Y.Z. Development builds on main use a -dev suffix.
-var Version = "v0.1.2-dev"
+var Version = "v0.1.3-dev"
